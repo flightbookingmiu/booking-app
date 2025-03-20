d144eec2e58c2f6ba60d13af359dba50f799f393
vmercel <vmercel@gmail.com>
2025-03-20 06:22:08 -0500

    new updates

--- backend/src/main/java/edu/miu/cs/cs425/backend/BackendApplication.java
+++ backend/src/main/java/edu/miu/cs/cs425/backend/BackendApplication.java
@@ -1,9 +1,8 @@
 package edu.miu.cs.cs425.backend;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
+
 @SpringBootApplication
 public class BackendApplication {
 
