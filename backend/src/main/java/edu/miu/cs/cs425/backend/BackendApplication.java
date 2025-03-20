ee2ed2dcc5fa2d038b913ca2e07f929c0eaadae1
vmercel <vmercel@gmail.com>
2025-03-20 06:05:04 -0500

    added files

--- backend/src/main/java/edu/miu/cs/cs425/backend/BackendApplication.java
+++ backend/src/main/java/edu/miu/cs/cs425/backend/BackendApplication.java
@@ -3,6 +3,7 @@ package edu.miu.cs.cs425.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootTest
 @SpringBootApplication
 public class BackendApplication {
 
